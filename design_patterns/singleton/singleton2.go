07111036bafc6433c26dccd9653856e91ef443e8
Edson Almeida <edson.ribeiro@onfly.com.br>
2026-01-02 08:46:08 -0300

    COOMIT

--- design_patterns/singleton/singleton2.go
+++ design_patterns/singleton/singleton2.go
@@ -3,54 +3,28 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 type Config struct {
-	Version string
-	mu      sync.Mutex
+	AppMode string
 }
 
 var (
-	configInstance *Config
-	onces          sync.Once
+	config *Config
+	onces  sync.Once
 )
 
 func GetConfig() *Config {
 	onces.Do(func() {
-		configInstance = &Config{
-			Version: "1.0",
-		}
+		config = &Config{}
 	})
-	return configInstance
-}
-
-func (c *Config) SetVersion(v string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	fmt.Println("Atualizando versão para:", v)
-	time.Sleep(time.Millisecond * 500)
-	c.Version = v
+	return config
 }
 
 func main() {
-	config := GetConfig()
+	c1 := GetConfig()
+	c1.AppMode = "Dark"
+	c2 := GetConfig()
+	fmt.Println(c2.AppMode)
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		config.SetVersion("2.0")
-	}()
-
-	go func() {
-		defer wg.Done()
-		config.SetVersion("3.0")
-	}()
-
-	wg.Wait()
-
-	fmt.Println("Versão final:", config.Version)
 }
---
+++ test_driven_development/calculator.go
@@ -0,0 +1,9 @@
+package test_driven_development
+
+func Sum(a int, b int) int {
+	return a + b
+}
+
+func calculatorTotal(qtd int, preco float64) float64 {
+	return float64(qtd) * preco
+}
