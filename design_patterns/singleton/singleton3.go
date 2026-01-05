ed4d70fff1db814a246ac0feb7e42364f254cd58
Edson Almeida <edson.ribeiro@onfly.com.br>
2026-01-05 08:36:36 -0300

    desingn patterns

--- design_patterns/singleton/singleton3.go
+++ design_patterns/singleton/singleton3.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Databases struct {
+	UrlConexao string
+}
+
+var (
+	instanc *Databases
+	onc     sync.Once
+)
+
+func GetDatabases() *Databases {
+	onc.Do(func() {
+		instanc = &Databases{
+			UrlConexao: "postgres://localhost:5432",
+		}
+	})
+	return instanc
+}
+
+func main() {
+	database := GetDatabases()
+	fmt.Println(database)
+}
