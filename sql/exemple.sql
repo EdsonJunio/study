984d0177da93a13e468a831fe2826aec3db90379
Edson Almeida <edson.ribeiro@onfly.com.br>
2026-01-09 08:33:02 -0300

    sql-03

--- sql/exemple.sql
+++ sql/exemple.sql
@@ -1,6 +1,6 @@
 SELECT p.name, COUNT(s.id) AS total_assinaturas
 FROM plans AS p
-INNER JOIN subscriptions AS s ON s.plan_id = p.id
+         INNER JOIN subscriptions AS s ON s.plan_id = p.id
 WHERE s.deleted_at IS NULL
 GROUP BY p.name
 ORDER BY total_assinaturas DESC;
@@ -8,7 +8,7 @@ ORDER BY total_assinaturas DESC;
 -- -------------------------------------------------
 SELECT c.name, SUM(s.total_amount) AS total_amount
 FROM customers AS c
-INNER JOIN sales AS s on s.customer_id = c.id
+         INNER JOIN sales AS s on s.customer_id = c.id
 WHERE s.deleted_at IS NULL
 GROUP BY c.name
 ORDER BY total_amount DESC
@@ -17,7 +17,7 @@ limit 3;
 -- --------------------------------------------------
 SELECT p.name, COUNT(s.id) AS sumplans
 FROM plans AS p
-INNER JOIN subscriptions AS s ON s.plan_id = p.id
+         INNER JOIN subscriptions AS s ON s.plan_id = p.id
 WHERE s.deleted_at IS NULL
 GROUP BY p.name
 HAVING sumplans > 1;
@@ -25,7 +25,31 @@ HAVING sumplans > 1;
 -- ---------------------------------------------------------------------
 SELECT c.name, SUM(s.total_amount) AS total_gasto
 FROM customers AS c
-INNER JOIN sales AS s ON s.customer_id = c.id
+         INNER JOIN sales AS s ON s.customer_id = c.id
 WHERE s.deleted_at IS NULL
 GROUP BY c.name
 HAVING total_gasto > 100000;
+
+-- -------------------------------------------------------------
+SELECT c.name, p.name
+FROM customers as c
+         INNER JOIN subscriptions AS s ON s.customer_id = c.id
+         INNER JOIN plans AS p ON s.plan_id = p.id
+WHERE s.deleted_at IS NULL;
+
+-- -------------------------------------------------------------
+SELECT c.name, p.name
+FROM customers as c
+         INNER JOIN subscriptions AS s ON s.customer_id = c.id
+         INNER JOIN plans AS p ON s.plan_id = p.id
+WHERE  s.deleted_at IS NULL && p.name LIKE '%Monthly%';
+
+-- -------------------------------------------------------------
+SELECT t.name, COUNT(c.id) AS QuantidadeDeClientes
+FROM tenants AS t
+INNER JOIN customers AS c ON c.tenant_id = t.id
+WHERE c.deleted_at IS NULL
+AND t.active = 1
+GROUP BY t.name
+order by QuantidadeDeClientes DESC
+LIMIT 1;
