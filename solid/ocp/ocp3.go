6022d0002875fa83cac8feca9a06ea977ae0dcaf
Edson Almeida <edson.ribeiro@onfly.com.br>
2026-03-09 18:35:19 -0300

    dia 6

--- solid/ocp/ocp3.go
+++ solid/ocp/ocp3.go
@@ -1,1 +1,51 @@
 package ocp
+
+import "fmt"
+
+type (
+	ServiceNotification struct {
+		Message string
+	}
+
+	Notification interface {
+		Send(s *ServiceNotification) error
+	}
+
+	EmailNotification struct{}
+
+	SMSNotification struct{}
+
+	NotificationOrchestrator struct{}
+)
+
+func (e *EmailNotification) Send(s *ServiceNotification) error {
+	fmt.Println("Sending E-mail corporation:", s.Message)
+	return nil
+}
+
+func (sms *SMSNotification) Send(s *ServiceNotification) error {
+	fmt.Println("Sending SMS via operator:", s.Message)
+	return nil
+}
+
+func (no *NotificationOrchestrator) Send(s *ServiceNotification, n Notification) error {
+	err := n.Send(s)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	notification := &ServiceNotification{
+		Message: "Your transfer of R$ 5,000 has been approved",
+	}
+
+	not := NotificationOrchestrator{}
+	err := not.Send(notification, &EmailNotification{})
+	if err != nil {
+		fmt.Println("Erro:", err)
+	}
+
+}
--- solid/ocp/ocp4.go
+++ solid/ocp/ocp4.go
@@ -1,1 +1,67 @@
 package ocp
+
+import "fmt"
+
+type (
+	Report struct {
+		Data []string
+	}
+
+	Exporter interface {
+		Export(r *Report) error
+	}
+
+	ReportCSV struct{}
+
+	ReportPDF struct{}
+
+	ReportJSON struct{}
+
+	ReportService struct{}
+)
+
+func (r *ReportCSV) Export(re *Report) error {
+	fmt.Println("Exporting data in CSV format (comma-separated)...")
+	for _, linha := range re.Data {
+		fmt.Println(linha)
+	}
+
+	return nil
+}
+
+func (r *ReportPDF) Export(re *Report) error {
+	fmt.Println("Exporting data in PDF format (with tables and logo)...")
+	for _, linha := range re.Data {
+		fmt.Println(linha)
+	}
+
+	return nil
+}
+
+func (r *ReportJSON) Export(re *Report) error {
+	fmt.Println("Exporting data in JSON format...")
+
+	return nil
+}
+
+func (r *ReportService) Export(re *Report, e Exporter) error {
+	err := e.Export(re)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	export := &Report{
+		Data: []string{"This is an export to test", "hello"},
+	}
+
+	service := ReportService{}
+	err := service.Export(export, &ReportCSV{})
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+}
